fdefb11301006b092ec1089d3bff60baefe97d84
SubashGitLogin <subash007official@gmail.com>
2022-05-05 07:47:36 -0700

    A Added codes

--- src/org/Sample/EmpDetails.java
+++ src/org/Sample/EmpDetails.java
@@ -1,12 +1,17 @@
 package org.Sample;
 
 public class EmpDetails {
+	int a=100;
+	int b=50;
 
 private void empId(int id) {
 	System.out.println("Emp ID :"+id);
 }
 private void companyName(String name) {
 System.out.println("Company Name :"+name);
+}
+private void print() {
+	System.out.println("Print Sucessful....");
 }
 public static void main(String[]args) {
 	EmpDetails d=new EmpDetails();
